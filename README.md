8ed4bf0ffc850d09f3c1a13e3953155f5be76207
Jakub Wlodarczyk <wlodarczykj1027@gmail.com>
2018-04-09 00:32:01 -0500

    Added capturing data.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SelfDrivingFZero
-Building an AI to play the SNES game FZero.
+Building an AI to play the SNES game FZero
--- capture_data.py
+++ capture_data.py
@@ -10,14 +10,38 @@ bbox = (0, 175, 501, 380)
 
 sct = mss()
 
+forward = [1,0,0,0]
+back = [0,1,0,0]
+left = [0,0,1,0]
+right = [0,0,0,1]
+
+output_dict = {
+    keyboard.Key.left : left,
+    keyboard.Key.right : right,
+    keyboard.Key.up : forward,
+    keyboard.Key.down : back
+}
+curr_output = None
+
+def training_output(key):
+    if(key in output_dict):
+        return output_dict[key]
+    else:
+        return [0,0,0,0]
+
 def on_press(key):
     try: k = key.char 
     except: k = key.name 
-    if key == keyboard.Key.esc: return False 
-    print('Key pressed: ' + k)
+    global curr_output
+    curr_output = training_output(key)
+    # print('Key pressed: ' + k)
 
 lis = keyboard.Listener(on_press=on_press)
 lis.start()
+
+training_data = []
+file_name = "D:/Projects/SelfDrivingFZero/training.data"
+saved = False
 
 while True:
     before = time.time()
@@ -25,12 +49,24 @@ while True:
     img = sct.grab(bbox)
     np_img = np.array(img)
     gray = cv2.cvtColor(np_img, cv2.COLOR_BGR2GRAY)
+
+    #Janky need a better way to do this.
+    if(curr_output):
+        training_data.append([gray, curr_output])
+
+    if(len(training_data) == 1 and not saved):
+        np.save(file_name, training_data)
+        saved = True
+
+    curr_output = None
     edges = cv2.Canny(gray, 50, 120)
     cv2.imshow('window', edges)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
+        lis.stop()
         break
+        
     after = time.time()
 
     # print("inner loop took : " + str(round((after - before),4) * 1000) + " ms")
